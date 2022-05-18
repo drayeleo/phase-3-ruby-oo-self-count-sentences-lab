fd9e42dcb15a68435e80e6b6ca4163ef025e9c43
Dylan Raye-Leonard <drayeleonard@gmail.com>
2022-05-18 16:13:58 -0600

    commit tree 8ad2c0a4aac1515c6e3dd5e34d870a02fd75c61a

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    result1 = self.split(".")
+    result2 = result1.map {
+      |substring| 
+      # binding.pry
+      substring.split("!") 
+    }.flatten
+    result3 = result2.map {
+      |substring| 
+      # binding.pry
+      substring.split("?") 
+    }.flatten
 
+    result3.length
+
+    # binding.pry
   end
 end
+
+str = "Hey. How's it going? Omg!!"
+
+str.count_sentences
